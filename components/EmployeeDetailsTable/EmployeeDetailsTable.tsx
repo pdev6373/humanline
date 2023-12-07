65fad236b440c2878ff231a631163dc54b40c493
pdev6373 <pdev6373@gmail.com>
2023-12-07 08:24:15 +0100

    made update

--- components/EmployeeDetailsTable/EmployeeDetailsTable.tsx
+++ components/EmployeeDetailsTable/EmployeeDetailsTable.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import styles from "./EmployeeDetailsTable.module.css";
-
 import { EmployeeDetailsListType } from "@/types";
 
 export default function EmployeeDetailsTable({
